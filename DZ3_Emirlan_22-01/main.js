d7b99929421a545e3c8fc6a45265803ec2734d31
Emirlan07055 <122383421+Emirlan07055@users.noreply.github.com>
2023-01-14 14:12:58 +0600

    сделал дз 3

---
+++ DZ3_Emirlan_22-01/main.js
@@ -0,0 +1,42 @@
+// function number  (a, b) {
+//     if (a < b) {
+//         console.log("Минимальное число: ", a)
+//     }else if (a > b){
+//         console.log("Минимальное число: ", b)
+//     }else if (b === a) {
+//         console.log("число ", a + "и" + b + "ровны")
+//     }else {
+//         alert('ERROR')
+//     }
+// }
+// number(91, 21)
+
+/*********************/
+
+// let str = (text = prompt('Введите текст')) => {
+//     return text.length
+// }
+// console.log(str())
+
+/*********************/
+
+// let calc = Number(prompt('Введите 1 число'))
+// let symbol = prompt('+, -, *, /')
+// let calculate = Number(prompt('Введите 2 число'))
+//
+// const number = (a, b ,c)  => {
+//     if (b==="+") {
+//         console.log(a + c)
+//     }else if (b==="-"){
+//         console.log(a - c)
+//     }else if (b==="*") {
+//         console.log(a * c)
+//     }else if (b==="/") {
+//         console.log(a / c)
+//     }else {
+//         alert('ERROR')
+//     }
+// }
+//
+// number(calc, symbol, calculate)
+
